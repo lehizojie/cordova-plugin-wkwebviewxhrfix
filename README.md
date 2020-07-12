263aad07deafbfecfd92da0fd1aa530771a76faf
lehizojie <60249465+lehizojie@users.noreply.github.com>
2020-07-12 11:50:00 -0600

    Fork to work with cordova-ios@6.x.x

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # cordova-plugin-wkwebviewxhrfix
 Cordova plugin to fix local file access via XHR with WKWebView
+Version 2.0.0 works with Cordova iOS v6.x.x
+  - For this you will also need to set the CDVWebViewEngine.h in CordovaLib.xcodeproj from Private to Public
 
 ## Installation
 
--- src/ios/CDVWebViewEngine+FileXhrFix.m
+++ src/ios/CDVWebViewEngine+FileXhrFix.m
@@ -1,25 +1,25 @@
 //
-//  CDVWKWebViewEngine+FileXhrFix.m
+//  CDVWebViewEngine+FileXhrFix.m
 //  HelloCordova
 //
 //  Created by Connor Pearson on 2/9/17.
 //
 //
 
-#import "CDVWKWebViewEngine+FileXhrFix.h"
+#import "CDVWebViewEngine+FileXhrFix.h"
 #import <objc/runtime.h>
 
-@implementation CDVWKWebViewEngine (FileXhrFix)
+@implementation CDVWebViewEngine (FileXhrFix)
 + (void)load {
     SEL selector = NSSelectorFromString(@"createConfigurationFromSettings:");
-    Method originalMethod = class_getInstanceMethod([CDVWKWebViewEngine class], selector);
+    Method originalMethod = class_getInstanceMethod([CDVWebViewEngine class], selector);
     IMP originalImp = method_getImplementation(originalMethod);
-    typedef WKWebViewConfiguration* (*send_type)(id, SEL , NSDictionary*);
+    typedef WebViewConfiguration* (*send_type)(id, SEL , NSDictionary*);
     send_type originalImpSend = (send_type)originalImp;
     
     IMP newImp = imp_implementationWithBlock(^(id _self, NSDictionary* settings){
         // Get the original configuration
-        WKWebViewConfiguration* configuration = originalImpSend(_self, selector, settings);
+        WebViewConfiguration* configuration = originalImpSend(_self, selector, settings);
 
         // allow access to file api
         @try {
